280aee8be3dfb8fa311dea79fbcf0a8881beadaa
Narender Rao Surabhi <77994312+narendersurabhi@users.noreply.github.com>
2024-10-13 20:58:38 -0400

    Refactor to use variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,53 +11,27 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] # Bitnami
 }
 
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
-
-/*
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  assign_ipv6_address_on_creation = "true"
-
-  tags = {
-    Name = "Main"
-  }
-}
-*/
-
-/*
-resource "aws_launch_configuration" "blog_template" {
-  name_prefix     = "aws-asg-"
-  image_id        = data.aws_ami.app_ami.id
-  instance_type   = var.instance_type  
-  //user_data       = filebase64("user-data-2.sh")
-  security_groups = [aws_security_group.sg_web.id] 
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-*/
 
 resource "aws_launch_template" "blog_template" {
-  name_prefix   = "aws-launch-template-"
+  name_prefix   = "${var.environment.name}-"
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
@@ -78,10 +52,11 @@ resource "aws_launch_template" "blog_template" {
 
 resource "aws_autoscaling_group" "blog_asg" {
   //availability_zones = ["us-east-2a", "us-east-2b"]
-  desired_capacity   = 2
-  max_size           = 4
-  min_size           = 2
-  name = "blog"
+  desired_capacity   = var.asg_desired_capacity
+  max_size           = var.asg_max_size
+  min_size           = var.asg_min_size
+
+  name = "${var.environment.name}-blog"
 
   vpc_zone_identifier   = module.blog_vpc.public_subnets
   //security_groups = [aws_security_group.sg_web.id]   
@@ -102,7 +77,7 @@ resource "aws_autoscaling_group" "blog_asg" {
 
   tag {
     key                 = "Environment"
-    value               = "dev"
+    value               = var.environment.name
     propagate_at_launch = true
   }
 
@@ -113,7 +88,7 @@ resource "aws_autoscaling_group" "blog_asg" {
 
 
 resource "aws_lb" "blog_alb" {
-  name               = "blog-alb"
+  name               = "${var.environment.name}-blog-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.sg_lb.id]
@@ -122,7 +97,7 @@ resource "aws_lb" "blog_alb" {
 }
 
 resource "aws_lb_target_group" "blog_tg" {
-  name     = "blog-target-group"
+  name     = "${var.environment.name}-blog-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.blog_vpc.vpc_id
@@ -139,7 +114,7 @@ resource "aws_autoscaling_attachment" "auto_to_targ" {
 }
 
 resource "aws_security_group" "sg_lb" {
-  name = "sg_lb"
+  name = "${var.environment.name}_blog_sg_lb"
   ingress {
     from_port   = 80
     to_port     = 80
@@ -160,7 +135,7 @@ resource "aws_security_group" "sg_lb" {
 }
 
 resource "aws_security_group" "sg_web" {
-  name = "web-sg"
+  name = "${var.environment.name}-blog-web-sg"
   ingress {
     from_port       = 80
     to_port         = 80
@@ -193,111 +168,3 @@ resource "aws_lb_listener" "blog_lb_lstnr" {
 
   depends_on         = [aws_lb.blog_alb, aws_lb_target_group.blog_tg]
 }
-
-
-/*
-resource "aws_lb_target_group" "blog_tg" {
-  name     = "blog-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.blog_vpc.vpc_id
-}
-*/
-
-/*
-resource "aws_lb_target_group" "blog_tg" {
-  name     = "blog-lb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.blog_vpc.vpc_id
-
-  target_group_health {
-    dns_failover {
-      minimum_healthy_targets_count      = "1"
-      minimum_healthy_targets_percentage = "off"
-    }
-
-    unhealthy_state_routing {
-      minimum_healthy_targets_count      = "1"
-      minimum_healthy_targets_percentage = "off"
-    }
-  }
-}
-*/
-
-/*
-module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
-
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-
-  # Security Group
-  security_groups = [module.blog_lb_sg.security_group_id]
-
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }    
-  }
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = module.blog_asg.autoscaling_group_id
-    }
-  }  
-
-  tags = {
-    Environment = "dev"    
-  }
-}
-*/
-
-/*
-module "blog_lb_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.2.0"
-  name = "blog_lb_sg"
-  
-  ingress_rules = [ "http-80-tcp", "https-443-tcp" ]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-
-  vpc_id = module.blog_vpc.vpc_id
-}
-
-module "blog_as_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.2.0"
-  name = "blog_as_sg"
-  
-  ingress_rules = [ "http-80-tcp", "https-443-tcp" ]
-  ingress_cidr_blocks = [module.blog_lb_sg.security_group_id]
-  
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-
-  vpc_id = module.blog_vpc.vpc_id
-}
-*/
-
-/*
-resource "aws_launch_template" "foobar" {
-  name_prefix   = "foobar"
-  image_id      = "ami-1a2b3c"
-  instance_type = "t2.micro"
-}
-*/
