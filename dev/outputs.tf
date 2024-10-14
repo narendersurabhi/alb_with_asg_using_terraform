08aaf74f4f929356cd8c94b47b1d595f272cec6f
Narender Rao Surabhi <77994312+narendersurabhi@users.noreply.github.com>
2024-10-14 13:36:17 -0400

    created dev and qa env

---
+++ dev/outputs.tf
@@ -0,0 +1,3 @@
+output "environment_url" {
+    value = module.dev.alb_dns_name
+}
--- qa/main.tf
+++ qa/main.tf
@@ -2,10 +2,9 @@ module "dev" {
   source = "../modules/blog"
 
   environment = {
-    name = "dev"
-    network_prefix = "10.0"
+    name = "qa"
+    network_prefix = "10.1"
   }
   asg_min_size = 1
   asg_max_size = 1
-
 }
